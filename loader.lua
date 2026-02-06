93f5d9ba3143c2724ef7be7c8db7eef1e0f568d9
Nexus Hub <jovancioon@gmail.com>
2026-02-06 16:37:45 +0700

    Fix SupportedGames entry for Demonology (Lobby)

--- loader.lua
+++ loader.lua
@@ -26,7 +26,7 @@ local Config = {
     },
     
     SupportedGames = {
-        [18794863104] = {Name = "Demonology (Lobby)", Icon = "rbxassetid://6137321701", ScriptId = "c1b36fb2505c5cfb10cd11a28c9355c0"},
+        [18199615050] = {Name = "Demonology (Lobby)", Icon = "rbxassetid://6137321701", ScriptId = "c1b36fb2505c5cfb10cd11a28c9355c0"},
         [18794863104] = {Name = "Demonology (Game)", Icon = "rbxassetid://18199615050", ScriptId = "c1b36fb2505c5cfb10cd11a28c9355c0"},
         [1537690962] = {Name = "Blair", Icon = "rbxassetid://1537690962", ScriptId = "d4f4e1f4b5e1e4c3a6f7b8c9d0e1f2a3"},
     }
